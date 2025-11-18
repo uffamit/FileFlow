2ce2f1f0294e45c55962273b62a4b33f9c90460e
Amit Divekar <110670491+uffamit@users.noreply.github.com>
2025-11-18 08:44:52 +0000

    working on this

--- FileFlow/config.py
+++ FileFlow/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///cloud_storage.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///fileflow.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'user_files'
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max file upload
